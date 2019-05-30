2d9c3acc6f5499604cda2bbed36da0c286c943e5
stellarway <goyu1026@gmail.com>
2019-05-30 17:49:50 +0900

    no message

---
+++ mymodule/mymodule.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+def run():
+    print("시작 되었습니다.")
+    
+def end():
+    print("종료 되었습니다.")
+    
+def calc_sum(a,b):
+    return a+b
+    
+#run()
+#end() 
+    
+print("my_module",__name__)
---
+++ mymodule/myrun.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import mymodule
+
+total = mymodule.calc_sum(3,3)
+print(total)
+
+print(__name__)
+
+print("myrun",__name__)
+
+if __name__: =='__main__':
+    mymodule.calc(3,3)
